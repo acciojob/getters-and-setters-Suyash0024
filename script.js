5ca4b70dcda6375b25a4db7f5331c8a2c614cade
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-07-23 11:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Person {}
+class Person {
+	private _name: string;
+	private _age: number;
 
-class Student extends Person {}
+	constructor(name:string, age:number){
+		this._name = name;
+		this._age = age;
+	}
 
-class Teacher extends Person {}
+	get name(): string {
+    return this._name;
+  }
+
+	set age(): number {
+		return this._age
+	}
+
+	
+}
+
+class Student extends Person {
+	study(): void {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+	teach(): void {
+    console.log(`${this.name} is teaching in javascript`);
+  }
+}
+const student = new Student("Alice", 20);
+student.study(); 
+
+const teacher = new Teacher("Mr. John", 35);
+teacher.teach(); 
+
+teacher.age = 36; // Using the setter
 
 // Do not change the code below this line
 window.Person = Person;
